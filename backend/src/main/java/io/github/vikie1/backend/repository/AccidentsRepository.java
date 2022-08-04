6fa5a13a5ef5688d65d4d7bb1218f75fe2728848
Victor Mwangi <39300268+vikie1@users.noreply.github.com>
2022-08-04 18:15:30 +0300

    Added accidents model repository and service

--- backend/src/main/java/io/github/vikie1/backend/repository/AccidentsRepository.java
+++ backend/src/main/java/io/github/vikie1/backend/repository/AccidentsRepository.java
@@ -3,5 +3,14 @@ package io.github.vikie1.backend.repository;
 import io.github.vikie1.backend.model.AccidentModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface AccidentsRepository extends JpaRepository<AccidentModel, Long> {
+    default List<AccidentModel> findByAccidentDataTime(LocalDate date){
+        return findByAccidentDataTimeBetween(date.atStartOfDay(), date.plusDays(1).atStartOfDay());
+    }
+
+    List<AccidentModel> findByAccidentDataTimeBetween(LocalDateTime from, LocalDateTime to);
 }
--- backend/src/main/java/io/github/vikie1/backend/service/AccidentsService.java
+++ backend/src/main/java/io/github/vikie1/backend/service/AccidentsService.java
@@ -1,11 +1,44 @@
 package io.github.vikie1.backend.service;
 
+import io.github.vikie1.backend.model.AccidentModel;
 import io.github.vikie1.backend.repository.AccidentsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Service
 public class AccidentsService {
     @Autowired
     AccidentsRepository accidentsRepository;
+
+    // CREATE
+    public void add(AccidentModel accidentModel) {
+        accidentsRepository.save(accidentModel);
+    }
+
+    // READ
+    public AccidentModel get(long id) {
+        return accidentsRepository.findById(id).orElse(null);
+    }
+    public List<AccidentModel> get(){
+        return accidentsRepository.findAll();
+    }
+    public List<AccidentModel> getToday(){
+        return accidentsRepository.findByAccidentDataTime(LocalDate.now());
+    }
+    public List<AccidentModel> getByDate(LocalDate date){
+        return accidentsRepository.findByAccidentDataTime(date);
+    }
+
+    // UPDATE
+    public void update(AccidentModel accidentModel) {
+        accidentsRepository.save(accidentModel);
+    }
+
+    // DELETE
+    public void delete(long id) {
+        accidentsRepository.deleteById(id);
+    }
 }
