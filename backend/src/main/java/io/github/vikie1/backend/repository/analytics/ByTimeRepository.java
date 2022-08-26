e41e6e04e210af155deb323929be2d5e3935d0ec
Victor Mwangi <39300268+vikie1@users.noreply.github.com>
2022-08-26 12:47:26 +0300

    add time statistical class

--- backend/src/main/java/io/github/vikie1/backend/repository/analytics/ByTimeRepository.java
+++ backend/src/main/java/io/github/vikie1/backend/repository/analytics/ByTimeRepository.java
@@ -4,4 +4,6 @@ import io.github.vikie1.backend.model.analytics.ByTime;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ByTimeRepository extends JpaRepository<ByTime, Long> {
+    ByTime findByTimeSlot(String timeSlot);
+    boolean existsByTimeSlot(String timeSlot);
 }
