20d32f7a0693fcd27f741edd428d219a27ef8ef3
Victor Mwangi <39300268+vikie1@users.noreply.github.com>
2022-08-27 14:06:19 +0300

    complete header styling

--- web-frontend/src/components/Header.jsx
+++ web-frontend/src/components/Header.jsx
@@ -1,35 +1,57 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 import { useEffect } from "react";
 import { Link } from "react-router-dom";
 
 export const Header = ({ pageTitle }) => {
-    useTitle(pageTitle + ' | School Project');
+  useTitle(pageTitle + " | School Project");
+
+  // css styles
+  const { listStyle, navPosition, navSizing, linkStyle, textLeft } =
+    headerStyles();
+
   return (
     <>
       <title>{pageTitle} | School Project</title>
 
-      <header>
-        <ul>
-          <li>
-            <Link to={"/home"}>Home</Link>
-          </li>
-          <li>
-            <Link to={"/data"}>Accident Data</Link>
-          </li>
-          <li>
-            <Link to={"/analytics"}>Statistics</Link>
-          </li>
-          <li>
-            <Link to={"/about"}>About Us</Link>
-          </li>
-          <li>
-            <Link to={"contact"}>Contact</Link>
-          </li>
-        </ul>
+      <header css={navPosition}>
+        <div css={textLeft}>Accident<br />Tracker</div>
+
+        <nav css={navSizing}>
+          <ul css={listStyle}>
+            <li>
+              <Link to={"/home"} css={linkStyle}>
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to={"/data"} css={linkStyle}>
+                Accident Data
+              </Link>
+            </li>
+            <li>
+              <Link to={"/analytics"} css={linkStyle}>
+                Statistics
+              </Link>
+            </li>
+            <li>
+              <Link to={"/about"} css={linkStyle}>
+                About Us
+              </Link>
+            </li>
+            <li>
+              <Link to={"/contact"} css={linkStyle}>
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </nav>
       </header>
     </>
   );
 };
 
+// the title tag wasn't working for some reason and using react helmet for this project is an overkill
 export function useTitle(title) {
   useEffect(() => {
     const prevTitle = document.title;
@@ -39,3 +61,39 @@ export function useTitle(title) {
     };
   });
 }
+
+export const headerStyles = () => {
+  const listStyle = css`
+    list-style: none;
+    display: flex;
+    justify-content: space-around;
+    background-color: transparent;
+  `;
+  const navSizing = css`
+    padding: 2rem 0px;
+    grid-column-start: 2;
+    grid-column-end: 4;
+  `;
+  const navPosition = css`
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    width: 100%;
+  `;
+  const linkStyle = css`
+    color: white;
+    text-decoration: none;
+    :hover {
+      text-decoration: underline;
+    }
+  `;
+  const textLeft = css`
+    color: white;
+    grid-column-start: 1;
+    grid-column-end: 2;
+    place-self: center;
+    font-size: x-large;
+    font-weight: bolder;
+  `;
+
+  return { listStyle, navPosition, navSizing, linkStyle, textLeft };
+};
--- web-frontend/src/pages/HomePage.jsx
+++ web-frontend/src/pages/HomePage.jsx
@@ -1,9 +1,31 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 import { Header } from "../components/Header";
+import backgroundImage from "./../resources/image/ewscripps.brightspotcdn.png";
 
 export const HomePage = (props) => {
+  const { background } = homePageStyles();
+
   return (
     <>
-      <Header pageTitle={'Home'}/>
+      <div css={background}>
+        <Header pageTitle={"Home"} />
+        <main></main>
+      </div>
     </>
   );
+};
+
+export const homePageStyles = () => {
+  const background = css`
+    width: 100vw;
+    height: 100vh;
+    background-image: url(${backgroundImage});
+    background-color: #200335;
+    background-attachment: fixed;
+    background-size: cover;
+    background-blend-mode: multiply;
+  `;
+
+  return { background };
 };
