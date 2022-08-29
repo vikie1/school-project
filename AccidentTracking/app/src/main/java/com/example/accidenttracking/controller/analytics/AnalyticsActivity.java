b995bcfecfea0f67fac76fda26573e124b486025
Victor Mwangi <39300268+vikie1@users.noreply.github.com>
2022-08-29 14:13:52 +0300

    add fragments i need for data visualisation

--- AccidentTracking/app/src/main/java/com/example/accidenttracking/controller/analytics/AnalyticsActivity.java
+++ AccidentTracking/app/src/main/java/com/example/accidenttracking/controller/analytics/AnalyticsActivity.java
@@ -1,10 +1,20 @@
 package com.example.accidenttracking.controller.analytics;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 
 import com.example.accidenttracking.R;
+import com.example.accidenttracking.controller.common.BottomNav;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 public class AnalyticsActivity extends AppCompatActivity {
 
@@ -12,5 +22,63 @@ public class AnalyticsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_analytics);
+
+        BottomNav.init(getWindow().getDecorView().getRootView(), this);
+        setUpTabLayout();
+    }
+
+    private void setUpTabLayout() {
+        SectionPagerAdapter sectionPagerAdapter = new SectionPagerAdapter(getSupportFragmentManager(), getLifecycle());
+        ViewPager2 pager = findViewById(R.id.pager);
+
+        pager.setAdapter(sectionPagerAdapter);
+        TabLayout tabLayout = findViewById(R.id.tabs);
+        new TabLayoutMediator(tabLayout, pager,
+                ((tab, position) -> {
+                    switch (position){
+                        case 0:
+                            tab.setText("Time");
+                            break;
+                        case 1:
+                            tab.setText("Area");
+                            break;
+                        case 2:
+                            tab.setText("Vehicles");
+                            break;
+                        case 3:
+                            tab.setText("Distribution");
+                            break;
+                    }
+                })
+        ).attach();
+    }
+
+    private static class SectionPagerAdapter extends FragmentStateAdapter {
+        FragmentManager fragmentManager;
+        public SectionPagerAdapter(FragmentManager fragmentManager, Lifecycle lifecycle) {
+            super(fragmentManager, lifecycle);
+            this.fragmentManager = fragmentManager;
+        }
+
+        @NonNull
+        @Override
+        public Fragment createFragment(int position) {
+            switch (position){
+                case 0:
+                    return new TimeChartFragment();
+                case 1:
+                    return new AreaFragment();
+                case 2:
+                    return new VehicleTypeFragment();
+                case 3:
+                    return new DistributionFragment();
+            }
+            return new TimeChartFragment();
+        }
+
+        @Override
+        public int getItemCount() {
+            return 4;
+        }
     }
 }
---
+++ AccidentTracking/app/src/main/java/com/example/accidenttracking/controller/analytics/TimeChartFragment.java
@@ -0,0 +1,21 @@
+package com.example.accidenttracking.controller.analytics;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.accidenttracking.R;
+
+public class TimeChartFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_time_chart, container, false);
+    }
+}
