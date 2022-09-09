3af3b0116625d5f84eb91a0c7cd51566f4316134
Victor Mwangi <39300268+vikie1@users.noreply.github.com>
2022-09-09 11:34:37 +0300

    add and TEST BAR CHART LIBRARY FOR REACTJS

--- web-frontend/src/pages/Statistics.jsx
+++ web-frontend/src/pages/Statistics.jsx
@@ -1,7 +1,24 @@
+import { BarChart, Bar, CartesianGrid, XAxis, YAxis } from "recharts";
+
 export const Statistics = (props) => {
-    return (
-        <>
-        Stats
-        </>
-    );
+  // Sample data
+  const data = [
+    { name: "Technical scripter", students: 700 },
+    { name: "Geek-i-knack", students: 200 },
+    { name: "Geeksforgeeks", students: 400 },
+    { name: "Geek-o-mania", students: 1000 },
+  ];
+  return (
+    <>
+      <h1>TEST BAR CHART LIBRARY FOR REACTJS</h1>
+      <div>
+        <BarChart width={600} height={600} data={data}>
+          <Bar dataKey="students" fill="green" />
+          {/* <CartesianGrid stroke="#ccc" /> */}
+          <XAxis dataKey="name" />
+          <YAxis />
+        </BarChart>
+      </div>
+    </>
+  );
 };
